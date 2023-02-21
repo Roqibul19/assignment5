c0115490dc9ffd92d000072795b3f0a36bf04a4f
Roqibul19 <haque15-5034@diu.edu.bd>
2023-02-22 03:59:56 +0600

    added first function

--- js/function.js
+++ js/function.js
@@ -1,5 +1,6 @@
-function inputValues (firstValue, secondValue){
-    const firstInputValue = document.getElementById(firstValue).value;
-    const SecondInputValue = document.getElementById(secondValue).value;
-    return firstInputValue, SecondInputValue;
+// function for 2nd and 3rd card
+function elementValues(value1,value2){
+    const cartdValue1=document.getElementById(value1).value;
+    const cardValue2= document.getElementById(value2).value;
+    return parseFloat(cartdValue1)*parseFloat(cardValue2);
 }
--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
+// first card
 document.getElementById('first-card').addEventListener('click',function(){
-    // first card
     const valueOfb = document.getElementById('b-value').value;
     const valueOfh = document.getElementById('h-value').value;
 
     const AreaTraingle= 0.5*parseFloat(valueOfb)*parseFloat(valueOfh);
+    console.log(AreaTraingle)
     
 })
+// click and calling function for 2nd card
+document.getElementById('second-card').addEventListener('click',function(){
+    firstCardArea= elementValues('l-value','w-value');
+    console.log(firstCardArea);
+    
+    
+})
+
