beb1e9b5fc9f1b9c651ea6a1975716544c8f03ee
Roqibul19 <haque15-5034@diu.edu.bd>
2023-02-22 05:53:23 +0600

    added 4th and 5th card in js

--- js/function.js
+++ js/function.js
@@ -1,6 +1,24 @@
-// function for 2nd and 3rd card
-function elementValues(value1,value2){
-    const cartdValue1=document.getElementById(value1).value;
-    const cardValue2= document.getElementById(value2).value;
-    return parseFloat(cartdValue1)*parseFloat(cardValue2);
+// common function for 2nd and 3rd card
+function elementValues23(value1,value2){
+    const cardValue1String=document.getElementById(value1);
+    const cardValue1=cardValue1String.value;
+    const cardValue2String= document.getElementById(value2);
+    const cardValue2= cardValue2String.value;
+
+    cardValue1String.value='';
+    cardValue2String.value='';
+
+    return parseFloat(cardValue1)*parseFloat(cardValue2);
+
+
+}
+// common function for 4th and 5th card
+function elementValues45(value1,value2){
+    const cardValue1String=document.getElementById(value1);
+    const cardValue1= cardValue1String.innerText;
+    const cardValue2String=document.getElementById(value2);
+    const cardValue2= cardValue2String.innerText;
+
+    return 0.5*parseFloat(cardValue1)*parseFloat(cardValue2);
+  
 }
--- js/index.js
+++ js/index.js
@@ -1,17 +1,40 @@
 // first card
 document.getElementById('first-card').addEventListener('click',function(){
-    const valueOfb = document.getElementById('b-value').value;
-    const valueOfh = document.getElementById('h-value').value;
+    const valueOfbString = document.getElementById('b-value');
+    const valueOfhString = document.getElementById('h-value');
+    const valueOfb=valueOfbString.value;
+    const valueOfh= valueOfhString.value;
 
     const AreaTraingle= 0.5*parseFloat(valueOfb)*parseFloat(valueOfh);
     console.log(AreaTraingle)
+
+    valueOfbString.value='';
+    valueOfhString.value='';
     
 })
 // click and calling function for 2nd card
 document.getElementById('second-card').addEventListener('click',function(){
-    firstCardArea= elementValues('l-value','w-value');
-    console.log(firstCardArea);
+    ariaRectangle= elementValues23('l-value','w-value');
+    console.log(ariaRectangle);
     
     
 })
+// click and calling function for 3rd card
+document.getElementById('3rd-card').addEventListener('click',function(){
+   ariaParallelogram= elementValues23('parallelogram-b-value','parallelogram-h-value');
+   console.log(ariaParallelogram);
+})
+
+// click and calling function for 4th card
+
+document.getElementById('4th-card').addEventListener('click',function(){
+    ariaRhombus= elementValues45('rhombus-d1-value','rhombus-d2-value');
+    console.log(ariaRhombus);
+ })
+// click and calling function for 5th card
+
+document.getElementById('5th-card').addEventListener('click',function(){
+    ariaPentagon= elementValues45('Pentagon-p-value','Pentagon-b-value');
+    console.log(ariaPentagon);
+ })
 
