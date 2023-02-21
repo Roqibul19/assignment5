f3e9bbae399637cba733d34ff84e3262041bcf17
Roqibul19 <haque15-5034@diu.edu.bd>
2023-02-22 03:19:37 +0600

    Js for first card

---
+++ js/function.js
@@ -0,0 +1,5 @@
+function inputValues (firstValue, secondValue){
+    const firstInputValue = document.getElementById(firstValue).value;
+    const SecondInputValue = document.getElementById(secondValue).value;
+    return firstInputValue, SecondInputValue;
+}
---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.getElementById('first-card').addEventListener('click',function(){
+    // first card
+    const valueOfb = document.getElementById('b-value').value;
+    const valueOfh = document.getElementById('h-value').value;
+
+    const AreaTraingle= 0.5*parseFloat(valueOfb)*parseFloat(valueOfh);
+    
+})
