1289110deb53afce5a18c8f3671257f6c89e90b6
Roqibul19 <haque15-5034@diu.edu.bd>
2023-02-22 08:57:50 +0600

    added validation

--- js/function.js
+++ js/function.js
@@ -1,13 +1,18 @@
 // common function for 2nd and 3rd card
 function elementValues23(value1,value2){
     const cardValue1String=document.getElementById(value1);
-    const cardValue1=cardValue1String.value;
     const cardValue2String= document.getElementById(value2);
+    const cardValue1=cardValue1String.value;
     const cardValue2= cardValue2String.value;
-
+    
     cardValue1String.value='';
     cardValue2String.value='';
-
+    // Card two and three validation
+    if(cardValue1<0 || cardValue2<0 || isNaN(cardValue1) || isNaN(cardValue2) || cardValue1=='' || cardValue2==''){
+        alert('Plz give valid number');
+        return;
+    }
+    
     return parseFloat(cardValue1)*parseFloat(cardValue2);
 
 
@@ -21,4 +26,25 @@ function elementValues45(value1,value2){
 
     return 0.5*parseFloat(cardValue1)*parseFloat(cardValue2);
   
+}
+
+// function for names
+function geometryNames(geoName){
+    const geometryName= document.getElementById(geoName).innerText;
+    return geometryName;
+}
+
+// function for display data
+function displayData(Name,Area){
+    const calculationContainer=document.getElementById('calculation-container');
+    const sectionn= document.createElement('section');
+    sectionn.innerHTML= `
+    <td>${serial}.</td> 
+    <td class="tab-name">${Name} </td> 
+    <td>${Area} </td> 
+    <button class="button-mitre">Convert to m<sup>2</sub></button>
+    `;
+    calculationContainer.appendChild(sectionn);
+
+
 }
--- js/index.js
+++ js/index.js
@@ -1,41 +1,59 @@
 // first card
+let serial=1;
 document.getElementById('first-card').addEventListener('click',function(){
     const valueOfbString = document.getElementById('b-value');
     const valueOfhString = document.getElementById('h-value');
     const valueOfb=valueOfbString.value;
     const valueOfh= valueOfhString.value;
-
-    const AreaTraingle= 0.5*parseFloat(valueOfb)*parseFloat(valueOfh);
-    console.log(AreaTraingle)
-
     valueOfbString.value='';
     valueOfhString.value='';
+    //   validation
+    if(valueOfb<0 || valueOfh<0 || isNaN(valueOfb) || isNaN(valueOfh) || valueOfb=='' || valueOfh==''){
+        alert('Plz give valid number');
+        return;
+    }
+    const Area= 0.5*parseFloat(valueOfb)*parseFloat(valueOfh);
     
+    const Name=geometryNames('name1');
+    displayData(Name, Area);
+
+    serial=serial+1;
 })
 // click and calling function for 2nd card
 document.getElementById('second-card').addEventListener('click',function(){
-    ariaRectangle= elementValues23('l-value','w-value');
-    console.log(ariaRectangle);
+    Area= elementValues23('l-value','w-value');
+    const Name=geometryNames('name2');
+    displayData(Name, Area);
     
+    serial=serial+1;
     
 })
 // click and calling function for 3rd card
 document.getElementById('3rd-card').addEventListener('click',function(){
-   ariaParallelogram= elementValues23('parallelogram-b-value','parallelogram-h-value');
-   console.log(ariaParallelogram);
+   Area= elementValues23('parallelogram-b-value','parallelogram-h-value');
+   const Name=geometryNames('name3');
+   displayData(Name, Area);
+
+   serial=serial+1;
 })
 
 // click and calling function for 4th card
 
 document.getElementById('4th-card').addEventListener('click',function(){
-    ariaRhombus= elementValues45('rhombus-d1-value','rhombus-d2-value');
-    console.log(ariaRhombus);
+    Area= elementValues45('rhombus-d1-value','rhombus-d2-value');
+    const Name=geometryNames('name4');
+    displayData(Name, Area);
+
+    serial =serial + 1;
  })
 // click and calling function for 5th card
 
 document.getElementById('5th-card').addEventListener('click',function(){
-    ariaPentagon= elementValues45('Pentagon-p-value','Pentagon-b-value');
-    console.log(ariaPentagon);
+    Area= elementValues45('Pentagon-p-value','Pentagon-b-value');
+    const Name=geometryNames('name5');
+    displayData(Name, Area);
+
+    serial = serial + 1;
  })
 
 //  6th or last card
@@ -46,6 +64,9 @@ document.getElementById('6th-card').addEventListener('click',function(){
     const valueOfaCard6=valueOfaStringCard6.innerText;
     const valueOfbCard6= valueOfbStringCard6.innerText;
 
-    const AreaEllipse= (3.14*parseFloat(valueOfaCard6).toFixed(2)*parseFloat(valueOfbCard6)).toFixed(2);
-    console.log(AreaEllipse);
+    const Area= (3.14*parseFloat(valueOfaCard6).toFixed(2)*parseFloat(valueOfbCard6)).toFixed(2);
+    const Name=geometryNames('name6');
+    displayData(Name, Area);
+
+    serial=serial+1;
  });
